6124b527907df3a0136061226c4cde51f1ef9220
alirueter <rueter.alison@gmail.com>
2020-08-11 18:18:27 -0500

    updated delete candidate

--- server.js
+++ server.js
@@ -34,43 +34,52 @@ const db = new sqlite3.Database('./db/election.db', err => {
 //     });
 // });
 
-//get a single candidate
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
+// //get a single candidate
+// app.get('/api/candidate/:id', (req, res) => {
+//     const sql = `SELECT * FROM candidates 
+//                  WHERE id = ?`;
+//     const params = [req.params.id];
+//     db.get(sql, params, (err, row) => {
+//       if (err) {
+//         res.status(400).json({ error: err.message });
+//         return;
+//       }
   
-      res.json({
-        message: 'success',
-        data: row
-      });
+//       res.json({
+//         message: 'success',
+//         data: row
+//       });
+//     });
+//   });
+
+//delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({error: res.message});
+            return;
+        }
+
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
     });
-  });
+});
 
-// //delete a candidate
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this, this.changes);
-// });
-
-// //create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// //ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// });
+//create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+//ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
+});
 
 //MUST GO LAST // default response for any other request(Not Found) catch all
 app.use((req, res) => {
