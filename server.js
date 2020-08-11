60d6d7ac0678d8b53e9fb4a09b87425336b857f4
alirueter <rueter.alison@gmail.com>
2020-08-11 18:12:20 -0500

    updated candidate route

--- server.js
+++ server.js
@@ -17,17 +17,40 @@ const db = new sqlite3.Database('./db/election.db', err => {
 });
 
 // //show all candidates 
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
+// app.get('/api/candidates', (req, res) => {
+//     const sql = `SELECT * FROM candidates`;
+//     const params = [];
+
+//     db.all(sql, params, (err, rows) => {
+//         if (err) {
+//             res.status(500).json({error: err.message});
+//             return;
+//         }
+
+//         res.json({
+//             message: 'success',
+//             data: rows
+//         });
+//     });
 // });
 
-// //GET a single candidate
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+//get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
 
 // //delete a candidate
 // db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
@@ -37,17 +60,17 @@ const db = new sqlite3.Database('./db/election.db', err => {
 //     console.log(result, this, this.changes);
 // });
 
-//create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-                VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
-//ES5 function, not arrow function, to use this
-db.run(sql, params, function(err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result, this.lastID);
-});
+// //create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// //ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
 
 //MUST GO LAST // default response for any other request(Not Found) catch all
 app.use((req, res) => {
